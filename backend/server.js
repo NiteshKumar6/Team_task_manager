c0fe979a5f308c34a7fb9889c4e1572898204628
NiteshKumar6 <kumarniteshsingh88640@gmail.com>
2026-05-08 00:17:41 +0530

    Script added to json

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,15 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.log(err));
 
+app.get("/", (req, res) => {
+    res.send("Backend is running");
+});
+
 app.use("/api/tasks", require("./routes/tasks"));
+app.use("/api/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
 });
-app.use("/api/auth", require("./routes/auth"));
