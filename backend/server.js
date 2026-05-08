e538bc50ea44c25ab42ff33bc31151a9dedddde4
NiteshKumar6 <kumarniteshsingh88640@gmail.com>
2026-05-08 11:41:35 +0530

    Fresh Deploy

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Backend is working");
+    res.send("Backend is running successfully");
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Running on ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
