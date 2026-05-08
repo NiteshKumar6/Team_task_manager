728941f3c9332223cc18f40f88a6a857cea0cf64
NiteshKumar6 <kumarniteshsingh88640@gmail.com>
2026-05-08 10:57:20 +0530

    Railway deploy fix

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log("MongoDB Connected"))
-    .catch(err => console.log(err));
-
 app.get("/", (req, res) => {
     res.send("Backend is running");
 });
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log(err));
 
 app.use("/api/tasks", require("./routes/tasks"));
 app.use("/api/auth", require("./routes/auth"));
